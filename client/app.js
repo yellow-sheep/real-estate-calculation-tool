a6ea52973139ee28698a6acd207aeb931bff567f
ryhoneydew <lucia.yang88@gmail.com>
2019-11-09 15:48:00 -0500

    first commit

---
+++ client/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+export default class App extends Component {
+  render() {
+    return <div>Welcome to React Boilerplate App</div>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
